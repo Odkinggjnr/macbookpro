06f7160ee6a2aaf7b1021a30a9736dd6cf80efb8
Odkinggjnr <chuksamitox@gmail.com>
2025-10-18 04:05:35 +0100

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ const Footer = () => {
     return (
         <footer>
             <div className="info">
-                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000800 040 1966.</p>
+                <p>More ways to shop: Find an Apple Store or other retailer near you. Or call +2349139104686.</p>
                 <img src="/logo.svg" alt ="Apple logo"/>
             </div>
 
             <hr />
 
             <div className="links">
-                <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+                <p>Copyright © 2025 Apple Inc. All rights reserved.</p>
 
                 <ul>
                     {footerLinks.map(({label, link }) => (
